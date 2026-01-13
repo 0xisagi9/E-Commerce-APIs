63a573eb18d0498b598fa284cae753ee9a042d39
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-13 17:16:00 +0200

    Create UserController.cs

---
+++ src/E-Commerce_APIs.API/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace E_Commerce_APIs.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost("signup", Name = "SignUp New User")]
+        public async Task<IActionResult> Signup([FromBody] User)
+        {
+
+        }
+    }
+}
