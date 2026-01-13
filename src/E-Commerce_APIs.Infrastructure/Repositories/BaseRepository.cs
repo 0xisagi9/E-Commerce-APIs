b3c4e0c08ba0efc424ab4d9ab6f5b941900a45e9
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-13 17:12:37 +0200

    Create BaseRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using E_Commerce_APIs.Shared.Interfaces;
+using E_Commerce_APIs.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class BaseRepository<T, Tkey> : IRepository<T, Tkey> where T : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+    public virtual async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+    public virtual void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+    public virtual void Remove(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public virtual async Task<bool> AnyAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AnyAsync(predicate);
+    }
+
+    public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<T?> GetByIdAsync(Tkey id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+}
