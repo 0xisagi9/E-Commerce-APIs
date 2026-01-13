9bb7b2ea1812877c6bf248dc7c16b4085d20ad96
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-13 17:15:17 +0200

    Some Updates

--- src/E-Commerce_APIs.API/obj/Debug/net8.0/E-Commerce_APIs.API.AssemblyInfo.cs
+++ src/E-Commerce_APIs.API/obj/Debug/net8.0/E-Commerce_APIs.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Commerce_APIs.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fe9f264a130aac8a7432aa41ef4eb13e95f6f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092ca8a7f76add6f31a2f240b942cefaa0c294ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Commerce_APIs.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Commerce_APIs.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
