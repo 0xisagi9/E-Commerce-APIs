8962e201c361e2d9cafd2d491a73d53f355d28f8
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-13 17:12:09 +0200

    Create IUnitOfWork.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IUnitOfWork : IDisposable
+{
+    IUserRepository User { get; }
+    Task<int> CompleteAsync();
+    void BeginTransaction();
+    Task CommitTransactionAsync();
+    void RollbackTransaction();
+}
+
