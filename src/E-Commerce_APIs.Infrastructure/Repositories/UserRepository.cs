f9a58b7f1d9a22864dd06ed025e1edcb59cf45de
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-13 17:12:36 +0200

    Create UserRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce_APIs.Shared.Interfaces;
+using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class UserRepository : BaseRepository<User, Guid>, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context) { }
+
+}
