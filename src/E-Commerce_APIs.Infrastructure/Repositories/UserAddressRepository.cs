880d62025c09015626246a9ac43711567067b3d7
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:21 +0200

    Create UserAddressRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/UserAddressRepository.cs
@@ -0,0 +1,22 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Infrastructure.Persistence.Context;
+using E_Commerce_APIs.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class UserAddressRepository : BaseRepository<UserAddress, int>, IUserAddressRepository
+{
+    public UserAddressRepository(AppDbContext context) : base(context) { }
+
+    public async Task<UserAddress?> GetDefaultAddressAsync(Guid userId) => await _dbSet
+                .Where(ua => ua.UserId == userId)
+                .OrderByDescending(ua => ua.CreationDate)
+                .FirstOrDefaultAsync();
+
+    public async Task<IEnumerable<UserAddress>> GetUserAddressesAsync(Guid userId) => await _dbSet
+                .Where(ua => ua.UserId == userId)
+                .OrderByDescending(ua => ua.CreationDate)
+                .ToListAsync();
+}
