88e76a01c273d3759bc6332ac951a432a695fb15
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:57 +0200

    Create IProductRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IProductRepository : IBaseRepository<Product, int>
+{
+    Task<Product?> GetByIdWithDetailsAsync(int id);
+    Task<Product?> GetBySlugAsync(string slug);
+    Task<IEnumerable<Product>> GetProductsByCategoryAsync(int categoryId);
+    Task<IEnumerable<Product>> GetProductsByBrandAsync(int brandId);
+    Task<IEnumerable<Product>> SearchProductsAsync(string searchTerm);
+    Task<IEnumerable<Product>> GetFeaturedProductsAsync(int count);
+    Task<IEnumerable<Product>> GetActiveProductsAsync();
+}
