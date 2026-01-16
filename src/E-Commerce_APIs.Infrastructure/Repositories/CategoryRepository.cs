e1faf4f791280b30651f5114c2bf56e67590a176
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:34 +0200

    Create CategoryRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿
+
+using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class CategoryRepository : BaseRepository<Category, int>, ICategoryRepository
+{
+    public CategoryRepository(DbContext context) : base(context) { }
+    public async Task<Category?> GetByIdWithChildrenAsync(int id) => await _dbSet
+            .Include(c => c.InverseParent)
+            .FirstOrDefaultAsync(c => c.Id == id && !c.IsDeleted);
+    public async Task<IEnumerable<Category>> GetRootCategoriesAsync() => await _dbSet
+            .Where(c => c.ParentId == null && !c.IsDeleted)
+            .Include(c => c.InverseParent)
+            .ToListAsync();
+    public async Task<IEnumerable<Category>> GetChildCategoriesAsync(int parentId) => await _dbSet
+            .Where(c => c.ParentId == parentId && !c.IsDeleted)
+            .ToListAsync();
+    public async Task<Category?> GetBySlugAsync(string slug) => await _dbSet
+            .FirstOrDefaultAsync(c => c.Slug == slug && !c.IsDeleted);
+}
