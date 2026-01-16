98147752f0e30cb159e3f4dbbfe63aad95eb0acc
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:38 +0200

    Create IVendorRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IVendorRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IVendorRepository : IBaseRepository<Vendor, Guid>
+{
+    Task<Vendor?> GetByEmailAsync(string email);
+    Task<IEnumerable<Vendor>> GetTopRatedVendorsAsync(int count);
+    Task<IEnumerable<Vendor>> GetActiveVendorsAsync();
+    Task<Vendor?> GetByIdWithOffersAsync(Guid id);
+}
