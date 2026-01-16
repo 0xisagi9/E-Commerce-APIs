ef77aa31fa7d5521aa7418f7738fe3a787e74338
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:47 +0200

    Create IUserAddressRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IUserAddressRepository.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IUserAddressRepository : IBaseRepository<UserAddress, int>
+{
+    Task<IEnumerable<UserAddress>> GetUserAddressesAsync(Guid userId);
+    Task<UserAddress?> GetDefaultAddressAsync(Guid userId);
+}
