7e09aa19bbea53d2c2cd2232202bf6f065492b1a
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:51 +0200

    Create IRoleRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IRoleRepository.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IRoleRepository : IBaseRepository<Role, int>
+{
+    Task<Role?> GetByNameAsync(string name);
+    Task<IEnumerable<Role>> GetUserRolesAsync(Guid userId);
+}
