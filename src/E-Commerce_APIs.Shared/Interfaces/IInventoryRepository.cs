1fbf78751facf0c3f3900e27ca10b2a83ad940b1
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:09 +0200

    Create IInventoryRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IInventoryRepository : IBaseRepository<Inventory, int>
+{
+    Task<Inventory?> GetByVendorOfferIdAsync(int vendorOfferId);
+    Task<int> GetAvailableQuantityAsync(int vendorOfferId);
+    Task<bool> ReserveQuantityAsync(int vendorOfferId, int quantity);
+    Task<bool> ReleaseReservedQuantityAsync(int vendorOfferId, int quantit);
+    Task<bool> ConfirmReservationAsync(int vendorOfferId, int quantity);
+    Task<IEnumerable<Inventory>> GetLowStockItemsAsync(int threshold);
+}
