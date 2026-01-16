209d27dc937381a83181480a230041eb59b54d18
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:03 +0200

    Create ICategoryRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface ICategoryRepository : IBaseRepository<Category, int>
+{
+    Task<Category?> GetByIdWithChildrenAsync(int id);
+    Task<IEnumerable<Category>> GetRootCategoriesAsync();
+    Task<IEnumerable<Category>> GetChildCategoriesAsync(int parentId);
+    Task<Category?> GetBySlugAsync(string slug);
+}
