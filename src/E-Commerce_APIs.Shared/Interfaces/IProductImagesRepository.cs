0884c173218ce6e4f0609b75a794cdc0760cf3e8
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:55 +0200

    Create IProductImagesRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IProductImagesRepository.cs
@@ -0,0 +1,9 @@
+﻿
+using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IProductImagesRepository : IBaseRepository<ProductImage, int>
+{
+    Task<IEnumerable<ProductImage>> GetProductImagesAsync(int productId);
+}
