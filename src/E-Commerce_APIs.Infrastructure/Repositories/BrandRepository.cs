640d73e58ced1107610685cfc2e13ca9fb31e3a2
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:37 +0200

    Create BrandRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/BrandRepository.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class BrandRepository : BaseRepository<Brand, int>, IBrandRepository
+{
+    public BrandRepository(DbContext context) : base(context) { }
+
+    public async Task<Brand?> GetByNameAsync(string name) => await _dbSet
+            .FirstOrDefaultAsync(b => b.Name == name && !b.IsDeleted);
+    public async Task<IEnumerable<Brand>> GetActiveBrandsAsync() => await _dbSet
+            .Where(b => !b.IsDeleted)
+            .OrderBy(b => b.Name)
+            .ToListAsync();
+}
