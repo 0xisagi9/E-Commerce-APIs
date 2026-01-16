bf299f7fd71130a445fca74c43336c13a32adc05
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:13 +0200

    Create PasswordHasher.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Services/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce_APIs.Shared.Interfaces;
+
+
+namespace E_Commerce_APIs.Infrastructure.Services;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+    public bool VerifyPassword(string password, string passwordhash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordhash);
+    }
+}
