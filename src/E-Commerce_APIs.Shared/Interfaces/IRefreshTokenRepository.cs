3f8e219c84469829826aee2a343d2d20d33ef3da
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:53 +0200

    Create IRefreshTokenRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IRefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IRefreshTokenRepository : IBaseRepository<RefreshToken, Guid>
+{
+    Task<RefreshToken?> GetByTokenAsync(string token);
+    Task<IEnumerable<RefreshToken>> GetActiveTokensByUserAsync(Guid userId);
+    Task RevokeUserTokensAsync(Guid userId);
+    Task CleanupExpiredTokensAsync();
+}
