c149a91c26b02607b60ca8bf57a24ccd6efed3a6
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:27 +0200

    Create RoleRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Infrastructure.Persistence.Context;
+using E_Commerce_APIs.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories
+{
+    public class RoleRepository : BaseRepository<Role, int>, IRoleRepository
+    {
+        public RoleRepository(AppDbContext context) : base(context) { }
+
+        public async Task<Role?> GetByNameAsync(string name) => await _dbSet.FirstOrDefaultAsync(r => r.Name == name);
+        public async Task<IEnumerable<Role>> GetUserRolesAsync(Guid userId) => await _context.Set<UserRole>()
+                .Where(ur => ur.UserId == userId)
+                .Select(ur => ur.Role)
+                .ToListAsync();
+    }
+}
