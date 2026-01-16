d1c015417a6fba4c3f17643947001f18d0d1cfc5
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:17 +0200

    Create VendorRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/VendorRepository.cs
@@ -0,0 +1,28 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class VendorRepository : BaseRepository<Vendor, Guid>, IVendorRepository
+{
+    public VendorRepository(DbContext context) : base(context) { }
+
+    public async Task<Vendor?> GetByEmailAsync(string email) => await _dbSet
+            .FirstOrDefaultAsync(v => v.Email == email && !v.IsDeleted);
+    public async Task<IEnumerable<Vendor>> GetTopRatedVendorsAsync(int count) => await _dbSet
+            .Where(v => !v.IsDeleted)
+            .OrderByDescending(v => v.AverageRate)
+            .Take(count)
+            .ToListAsync();
+    public async Task<IEnumerable<Vendor>> GetActiveVendorsAsync() => await _dbSet
+            .Where(v => !v.IsDeleted)
+            .OrderBy(v => v.Name)
+            .ToListAsync();
+    public async Task<Vendor?> GetByIdWithOffersAsync(Guid id) => await _dbSet
+            .Include(v => v.VendorOffers)
+                .ThenInclude(vo => vo.Product)
+            .Include(v => v.VendorOffers)
+                .ThenInclude(vo => vo.Inventory)
+            .FirstOrDefaultAsync(v => v.Id == id && !v.IsDeleted);
+}
