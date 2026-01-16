a4fec6860b36466fd4b6938d95a76d544b6cfc68
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:43 +0200

    Create IVendorOfferRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IVendorOfferRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IVendorOfferRepository : IBaseRepository<VendorOffer, int>
+{
+    Task<IEnumerable<VendorOffer>> GetOffersByProductAsync(int productId);
+    Task<IEnumerable<VendorOffer>> GetOffersByVendorAsync(Guid vendorId);
+    Task<VendorOffer?> GetCheapestOfferAsync(int productId);
+    Task<VendorOffer?> GetByIdWithInventoryAsync(int id);
+    Task<bool> HasActiveOffersAsync(int productId);
+}
