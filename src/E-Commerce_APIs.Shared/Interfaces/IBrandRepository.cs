f1c8f516b6511b6e94827fd19f5a5bfa39595c19
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:11 +0200

    Create IBrandRepository.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IBrandRepository.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IBrandRepository : IBaseRepository<Brand, int>
+{
+    Task<Brand?> GetByNameAsync(string name);
+    Task<IEnumerable<Brand>> GetActiveBrandsAsync();
+}
