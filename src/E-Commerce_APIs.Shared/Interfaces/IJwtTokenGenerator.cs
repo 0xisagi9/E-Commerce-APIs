8523a4d7ae9227e0df6b464f68d8da3a8171cbd3
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:00:59 +0200

    Create IJwtTokenGenerator.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+
+namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface IJwtTokenGenerator
+{
+    public string GenerateToken(User user);
+    public string GenerateRefreshToken();
+}
