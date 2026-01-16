19d6978ebed7c83c8bfb375e8353e07f29ec5d57
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-16 19:01:50 +0200

    Create ProductImagesRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/ProductImagesRepository.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Shared.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class ProductImagesRepository : BaseRepository<ProductImage, int>, IProductImagesRepository
+{
+    public ProductImagesRepository(DbContext context) : base(context) { }
+
+    public async Task<IEnumerable<ProductImage>> GetProductImagesAsync(int productId) => await _dbSet
+            .Where(pi => pi.ProductId == productId)
+            .OrderBy(pi => pi.CreationDate)
+            .ToListAsync();
+}
