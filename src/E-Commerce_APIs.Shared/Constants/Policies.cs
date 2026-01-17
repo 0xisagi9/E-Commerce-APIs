7fd3f4bd2fa37dd9aa08d07308adf6606e714bac
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:53:59 +0200

    Create Policies.cs

---
+++ src/E-Commerce_APIs.Shared/Constants/Policies.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce_APIs.Shared.Constants;
+
+public static class Policies
+{
+    public const string AdminOnly = "AdminOnly";
+    public const string CustomerOrAdmin = "CustomerOrAdmin";
+    public const string VendorOrAdmin = "VendorOrAdmin";
+    public const string ManagerOrAdmin = "ManagerOrAdmin";
+}
