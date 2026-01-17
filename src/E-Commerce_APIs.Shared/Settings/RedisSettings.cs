26c76aeec20f10b57639fd78fb15efc78abb763b
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:54:04 +0200

    Create RedisSettings.cs

---
+++ src/E-Commerce_APIs.Shared/Settings/RedisSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace E_Commerce_APIs.API.Configurations;
+
+public class RedisSettings
+{
+    public bool Enabled { get; set; }
+    public string ConnectionString { get; set; } = string.Empty;
+}
