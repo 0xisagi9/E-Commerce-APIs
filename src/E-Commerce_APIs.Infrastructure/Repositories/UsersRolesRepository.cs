39405b9e012d34893a175cc97355dba862f5bd2b
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:54:24 +0200

    Create UsersRolesRepository.cs

---
+++ src/E-Commerce_APIs.Infrastructure/Repositories/UsersRolesRepository.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Infrastructure.Persistence.Context;
+using E_Commerce_APIs.Shared.Interfaces;
+
+namespace E_Commerce_APIs.Infrastructure.Repositories;
+
+public class UsersRolesRepository : BaseRepository<UserRole, int>, IUsersRolesRepository
+{
+    public UsersRolesRepository(AppDbContext context) : base(context) { }
+}
