88c6ef6d670e28651dbce10a91204c153c5df971
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:53:57 +0200

    Create Roles.cs

---
+++ src/E-Commerce_APIs.Shared/Constants/Roles.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace E_Commerce_APIs.Shared.Constants;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Customer = "Customer";
+    public const string Vendor = "Vendor";
+    public const string Manager = "Manager";
+}
