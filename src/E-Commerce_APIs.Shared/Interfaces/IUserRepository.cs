58fecba6aeeef862834009c8eb051d4ebfc47e12
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:53:37 +0200

    Update IUserRepository.cs

--- src/E-Commerce_APIs.Shared/Interfaces/IUserRepository.cs
+++ src/E-Commerce_APIs.Shared/Interfaces/IUserRepository.cs
@@ -5,6 +5,7 @@ public interface IUserRepository : IBaseRepository<User, Guid>
 {
     Task<User?> GetByEmailAsync(string email);
     Task<User?> GetByUserNameAsync(string userName);
+    Task<User?> GetByPhoneNumberAsync(string phoneNumber);
     Task<User?> GetByIdWithRolesAsync(Guid id);
     Task<bool> IsEmailUniqueAsync(string email, Guid? excludeUserId = null);
     Task<IEnumerable<User>> GetVerifiedUsersAsync();
