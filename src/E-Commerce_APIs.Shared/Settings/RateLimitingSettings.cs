f6d81140cebf32db7f03e231ebd8776ff69340ba
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:54:06 +0200

    Create RateLimitingSettings.cs

---
+++ src/E-Commerce_APIs.Shared/Settings/RateLimitingSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce_APIs.API.Configurations;
+
+public class RateLimitingSettings
+{
+    public int PermitLimit { get; set; } = 100;
+    public int WindowInSeconds { get; set; } = 60;
+    public int QueueLimit { get; set; } = 2;
+}
