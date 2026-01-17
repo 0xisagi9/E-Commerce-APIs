b852877354b2056186b5194ab7f3d5fecaa2f1ce
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:54:11 +0200

    Create FeatureFlagsSettings.cs

---
+++ src/E-Commerce_APIs.Shared/Settings/FeatureFlagsSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace E_Commerce_APIs.API.Configurations;
+
+public class FeatureFlagsSettings
+{
+    public bool EnableSwagger { get; set; }
+    public bool EnableDetailedErrors { get; set; }
+}
