a09442d7f1460c934cb117e68d2af724f7eb95de
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-17 15:55:21 +0200

    Create AuthController.cs

---
+++ src/E-Commerce_APIs.API/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce_APIs.Application.DTOs;
+using E_Commerce_APIs.Application.Features.Users.Commands.RegisterUser;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_APIs.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AuthController(IMediator mediator) => _mediator = mediator;
+
+    [HttpPost("register")]
+    public async Task<ActionResult<AuthResponseDto>> Register([FromBody] RegisterUserCommand command)
+    {
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
