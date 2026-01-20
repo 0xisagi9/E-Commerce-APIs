ba434b5e41cf0b3898e41a4ef3e44767eb7878b4
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-20 14:59:38 +0200

    Create ICookieService.cs

---
+++ src/E-Commerce_APIs.Shared/Interfaces/ICookieService.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce_APIs.Shared.Interfaces;
+
+public interface ICookieService
+{
+    void SetRefreshToken(string refreshToken, DateTime expiredAt);
+    string GetRefreshToken();
+    void DeleteRefreshToken();
+}
