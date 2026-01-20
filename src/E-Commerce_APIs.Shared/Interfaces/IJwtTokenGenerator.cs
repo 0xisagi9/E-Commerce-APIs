cb4b97497e10472be1d1c990a12108148bac0479
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-20 14:59:41 +0200

    Update IJwtTokenGenerator.cs

--- src/E-Commerce_APIs.Shared/Interfaces/IJwtTokenGenerator.cs
+++ src/E-Commerce_APIs.Shared/Interfaces/IJwtTokenGenerator.cs
@@ -5,8 +5,8 @@ namespace E_Commerce_APIs.Shared.Interfaces;
 
 public interface IJwtService
 {
-    string GenerateAccessToken(User user, IEnumerable<string> roles);
+    string GenerateAccessToken(User user, string role);
     string GenerateRefreshToken();
     ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
-    Task<RefreshToken> CreateRefreshTokenAsync(Guid userId, string token, string? ipAddress, string? userAgent);
+    Task<RefreshToken> CreateRefreshTokenAsync(Guid userId, string token, string ipAddress, string userAgent);
 }
