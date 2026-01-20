d3cb3ff7d512bf85b3e94eea96dc1f025704043d
0xisagi9 <muhammed9alii9@gmail.com>
2026-01-20 14:59:44 +0200

    Create Result.cs

---
+++ src/E-Commerce_APIs.Shared/Helpers/Result.cs
@@ -0,0 +1,20 @@
+﻿namespace E_Commerce_APIs.Shared.Helpers;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public T Data { get; }
+    public string Message { get; }
+    public string? Error { get; }
+
+    private Result(bool isSuccess, T data, string? error, string message)
+    {
+        IsSuccess = isSuccess;
+        Data = data;
+        Error = error;
+        Message = message;
+    }
+
+    public static Result<T> Success(T data, string message) => new(true, data, null, message);
+    public static Result<T> Failure(string error, string message) => new(false, default!, error, message);
+}
