dc16458daa716d1298d951553f8fc28038f7db10
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-01 17:33:14 +0200

    Update RegisterUserCommand.cs

--- src/E-Commerce_APIs.Application/Features/Users/Commands/RegisterUser/RegisterUserCommand.cs
+++ src/E-Commerce_APIs.Application/Features/Users/Commands/RegisterUser/RegisterUserCommand.cs
@@ -1,13 +1,16 @@
-﻿using MediatR;
+﻿using E_Commerce_APIs.Application.DTOs;
+using MediatR;
 using System.ComponentModel.DataAnnotations;
+using E_Commerce_APIs.Shared.Helpers;
 namespace E_Commerce_APIs.Application.Features.Users.Commands.RegisterUser;
 
-public class RegisterUserCommand : IRequest<Guid>
+public class RegisterUserCommand : IRequest<Result<AuthResponseDto>>
 {
     public string UserName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public string ConfirmPassword { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
 }
