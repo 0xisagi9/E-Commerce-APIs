6570fa13bf67d39d0386aacd1621e9644621d023
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-01 17:33:23 +0200

    Create ValidationException.cs

---
+++ src/E-Commerce_APIs.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation.Results;
+using Microsoft.Extensions.FileProviders;
+
+namespace E_Commerce_APIs.Application.Exceptions;
+
+public class ValidationException : Exception
+{
+    public IDictionary<string, string[]> Errors { get; }
+
+    public ValidationException() : base("One or more validation failure have occurred.")
+    {
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+    {
+        Errors = failures
+           .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+           .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+    }
+}
