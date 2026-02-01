cb47245275777563b64ef09640fde8b35bb28444
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-01 17:33:29 +0200

    Create AuthResponseDto.cs

---
+++ src/E-Commerce_APIs.Application/DTOs/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce_APIs.Application.DTOs;
+namespace E_Commerce_APIs.Application.DTOs;
+
+public class AuthResponseDto
+{
+    public string? AccessToken { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+    public UserDto? User { get; set; }
+}
