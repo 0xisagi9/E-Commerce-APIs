3336208d2ae9a1b04929044d0e4f13ca1d127e26
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-01 17:33:57 +0200

    Update Program.cs
    Register new Services (AutoMapper, MediatR)

--- src/E-Commerce_APIs.API/Program.cs
+++ src/E-Commerce_APIs.API/Program.cs
@@ -1,11 +1,14 @@
 using AutoMapper;
 using E_Commerce_APIs.API.Configurations;
+using E_Commerce_APIs.Application.Behaviors;
 using E_Commerce_APIs.Application.Features.Users.Commands.RegisterUser;
+using E_Commerce_APIs.Application.Middleware;
 using E_Commerce_APIs.Infrastructure.Persistence.Context;
 using E_Commerce_APIs.Infrastructure.Persistence.UnitOfWork;
 using E_Commerce_APIs.Infrastructure.Services;
 using E_Commerce_APIs.Shared.Interfaces;
 using E_Commerce_APIs.Shared.Settings;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
@@ -37,72 +40,74 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Swagger Configuration with JWT Support
-// Swagger Configuration with JWT Support
-//builder.Services.AddSwaggerGen(options =>
-//{
-//    options.SwaggerDoc("v1", new OpenApiInfo
-//    {
-//        Title = "Multi Vendor E-Commerce API",
-//        Version = "v1",
-//        Description = "Multi-Vendor E-Commerce APIs with CQRS Pattern",
-//        Contact = new OpenApiContact
-//        {
-//            Name = "0xmuhammed9",
-//            Email = "muhammed9alii9@gmail.com"
-//        }
-//    });
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Multi Vendor E-Commerce API",
+        Version = "v1",
+        Description = "Multi-Vendor E-Commerce APIs with CQRS Pattern",
+        Contact = new OpenApiContact
+        {
+            Name = "0xmuhammed9",
+            Email = "muhammed9alii9@gmail.com"
+        }
+    });
 
-//    // Add JWT Authentication to Swagger
-//    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-//    {
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.Http,
-//        Scheme = "Bearer",
-//        BearerFormat = "JWT",
-//        In = ParameterLocation.Header,
-//        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\n\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
-//    });
+    // Add JWT Authentication to Swagger
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\n\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\""
+    });
 
-//    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-//    {
-//        {
-//            new OpenApiSecurityScheme
-//            {
-//                Reference = new OpenApiReference
-//                {
-//                    Type = ReferenceType.SecurityScheme,
-//                    Id = "Bearer"
-//                }
-//            },
-//            Array.Empty<string>()
-//        }
-//    });
-//});
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowAll", policy =>
-//    {
-//        policy.AllowAnyOrigin()
-//              .AllowAnyMethod()
-//              .AllowAnyHeader();
-//    });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
 
-//    options.AddPolicy("Production", policy =>
-//    {
-//        policy.WithOrigins("https://yourdomain.com", "https://www.yourdomain.com")
-//              .AllowAnyMethod()
-//              .AllowAnyHeader()
-//              .AllowCredentials();
-//    });
-//});
+    options.AddPolicy("Production", policy =>
+    {
+        policy.WithOrigins("https://yourdomain.com", "https://www.yourdomain.com")
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
 
 
 
 // Add MediatR and CQRS
 builder.Services.AddMediatR(typeof(RegisterUserCommand).Assembly);
+builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+builder.Services.AddValidatorsFromAssembly(typeof(RegisterUserValidator).Assembly);
+
+
 
 // Add AutoMapper
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
@@ -114,8 +119,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
