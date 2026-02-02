081493a89ebd768c6addf518a4d55dc062f2fe05
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-02 17:17:52 +0200

    feat(auth): add auth response builder interface

---
+++ src/E-Commerce_APIs.Application/Common/Interfaces/IAuthResponseBuilder.cs
@@ -0,0 +1,16 @@
+﻿using E_Commerce_APIs.Application.DTOs;
+using E_Commerce_APIs.Application.Services;
+using E_Commerce_APIs.Domain.Entities;
+
+
+namespace E_Commerce_APIs.Application.Common.Interfaces;
+
+/// <summary>
+/// Builds the authentication response DTO
+/// Separates response mapping logic from command handler
+/// </summary>
+public interface IAuthResponseBuilder
+{
+    AuthResponseDto BuildAuthResponse(User user, string roleName, AuthTokenDto authToken);
+}
+
