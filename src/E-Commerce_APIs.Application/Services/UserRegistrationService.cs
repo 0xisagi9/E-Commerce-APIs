be28ab28a518c03417ebf5e9bfae12d32bb5930f
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-02 17:14:14 +0200

    feat(register): add user registration service to handle register logic
    - Responsible for creating User and save it to database.
    - Responsible for assign roles to new user, create user role, and save to database.

---
+++ src/E-Commerce_APIs.Application/Services/UserRegistrationService.cs
@@ -0,0 +1,48 @@
+namespace E_Commerce_APIs.Application.Services;
+
+using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Infrastructure.Persistence.UnitOfWork;
+using E_Commerce_APIs.Shared.Constants;
+using E_Commerce_APIs.Shared.Interfaces;
+
+using E_Commerce_APIs.Application.Common.Interfaces;
+
+public class UserRegistrationService : IUserRegistrationService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UserRegistrationService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<User> CreateUserAsync(string userName, string email, string firstName, string lastName, string phoneNumber, string passwordHash)
+    {
+        var user = new User
+        {
+            UserName = userName,
+            Email = email,
+            FirstName = firstName,
+            LastName = lastName,
+            PhoneNumber = phoneNumber,
+            PasswordHash = passwordHash,
+        };
+
+        return await _unitOfWork.Users.AddAsync(user);
+    }
+
+    public async Task AssignCustomerRoleAsync(User user)
+    {
+        var role = await _unitOfWork.Roles.GetByNameAsync(Roles.Customer);
+        if (role == null)
+            throw new InvalidOperationException("Customer role not found");
+
+        var userRole = new UserRole
+        {
+            RoleId = role.Id,
+            UserId = user.Id,
+        };
+
+        await _unitOfWork.UsersRoles.AddAsync(userRole);
+    }
+}
