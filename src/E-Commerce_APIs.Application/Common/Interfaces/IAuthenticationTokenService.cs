54abce06a970b3ebb425d83ce9d107d777ac83e2
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-02 17:18:11 +0200

    feat(auth): add authentication token service interface

---
+++ src/E-Commerce_APIs.Application/Common/Interfaces/IAuthenticationTokenService.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce_APIs.Application.Services;
+using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Shared.Interfaces;
+using E_Commerce_APIs.Application.DTOs;
+
+
+namespace E_Commerce_APIs.Application.Common.Interfaces;
+
+/// <summary>
+/// Handles authentication token generation and refresh token creation
+/// Separates token logic from command handler
+/// </summary>
+public interface IAuthenticationTokenService
+{
+    Task<AuthTokenDto> GenerateAuthTokenAsync(User user, string roleName, IUnitOfWork unitOfWork);
+    void SetRefreshTokenCookie(string refreshToken, DateTime expiration, ICookieService cookieService);
+}
