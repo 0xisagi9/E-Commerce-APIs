ed18550c5b598830d0f12cd0746eb1cce2cf804d
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-02 17:19:22 +0200

    feat(auth): add user data transferee object
    - Helper object for user data

---
+++ src/E-Commerce_APIs.Application/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace E_Commerce_APIs.Application.DTOs;
+
+public class UserDto
+{
+    public Guid Id { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string? PhoneNumber { get; set; }
+    public bool IsVerified { get; set; }
+    public List<string> Roles { get; set; } = new();
+}
