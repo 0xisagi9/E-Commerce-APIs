39ba911c40349321a6949e812b6e49d28790d30d
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-02 17:19:35 +0200

    feat(auth): add auth token data transferee object

---
+++ src/E-Commerce_APIs.Application/DTOs/AuthTokenDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_APIs.Application.DTOs;
+
+public class AuthTokenDto
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime RefreshTokenExpiration { get; set; }
+    public DateTime ExpiresAt { get; set; }
+}
