46c2251f6f1e9a404edec0e69a99900f84c72a1f
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-03 16:33:28 +0200

    feat(admin): add admin controller

---
+++ src/E-Commerce_APIs.API/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using E_Commerce_APIs.Application.DTOs;
+using E_Commerce_APIs.Application.Features.Users.Queries.GetUsers;
+using E_Commerce_APIs.Shared.Helpers;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_APIs.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize(Roles = "Admin")]
+public class AdminController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public AdminController(IMediator mediator) => _mediator = mediator;
+
+    [HttpGet("users")]
+    [ProducesResponseType(typeof(PaginatedResult<UserDto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetUsers([FromQuery] GetUsersQuery query)
+    {
+        var result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
