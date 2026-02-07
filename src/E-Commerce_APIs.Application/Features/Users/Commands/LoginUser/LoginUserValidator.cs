4b3fafc97d8f11defdcf8cef383caea977b22395
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-07 16:44:34 +0200

    feat(auth): add Login user validator
    - Validate email and password

---
+++ src/E-Commerce_APIs.Application/Features/Users/Commands/LoginUser/LoginUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace E_Commerce_APIs.Application.Features.Users.Commands.LoginUser;
+
+public class LoginUserValidator : AbstractValidator<LoginUserCommand>
+{
+    public LoginUserValidator()
+    {
+        RuleFor(user => user.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email format.")
+            .MaximumLength(255).WithMessage("Email must not exceed 255 characters.");
+
+        RuleFor(user => user.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
+            .MaximumLength(100).WithMessage("Password must not exceed 100 characters.");
+    }
+}
