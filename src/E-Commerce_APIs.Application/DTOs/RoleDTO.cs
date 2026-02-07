14ce2e854b9ff8d408b5f3f59c05508b5c82418e
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-07 16:35:35 +0200

    feat(role): add Role dto

---
+++ src/E-Commerce_APIs.Application/DTOs/RoleDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce_APIs.Application.DTOs;
+
+public class RoleDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+}
