9a81dd7acfdd1dadba6e2f58695a92f9d9520ccf
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-07 16:34:18 +0200

    feat(auth): add auth response builder
    - Implement authResponseBuilder with Builder design pattern.

---
+++ src/E-Commerce_APIs.Application/Common/Builders/AuthResponseBuilder.cs
@@ -0,0 +1,29 @@
+﻿using E_Commerce_APIs.Application.Common.Interfaces;
+using E_Commerce_APIs.Domain.Entities;
+using E_Commerce_APIs.Application.DTOs;
+
+namespace E_Commerce_APIs.Application.Common.Builders;
+
+public class AuthResponseBuilder : IAuthResponseBuilder
+{
+    public AuthResponseDto BuildAuthResponse(User user, string roleName, AuthTokenDto authToken)
+    {
+        return new AuthResponseDto
+        {
+            User = new UserDto
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+                IsVerified = user.IsVerified,
+                Roles = new List<string> { roleName }
+            },
+            AccessToken = authToken.AccessToken,
+            ExpiresAt = authToken.ExpiresAt
+        };
+    }
+}
+
