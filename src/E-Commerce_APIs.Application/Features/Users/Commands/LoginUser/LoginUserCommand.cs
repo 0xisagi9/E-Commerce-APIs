50796b43fe85c05d67e5a8a2350bb0a66202c51a
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-07 16:34:58 +0200

    feat(auth): add Login user command

---
+++ src/E-Commerce_APIs.Application/Features/Users/Commands/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce_APIs.Application.DTOs;
+using MediatR;
+using E_Commerce_APIs.Shared.Helpers;
+
+namespace E_Commerce_APIs.Application.Features.Users.Commands.LoginUser;
+
+public class LoginUserCommand : IRequest<Result<AuthResponseDto>>
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
