fef9384ad25e7a64708c1a23ffd794f5af165686
0xisagi9 <muhammed9alii9@gmail.com>
2026-02-07 19:26:59 +0200

    feat(user): add get user by id query

---
+++ src/E-Commerce_APIs.Application/Features/Users/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using E_Commerce_APIs.Application.DTOs;
+using MediatR;
+using E_Commerce_APIs.Shared.Helpers;
+
+namespace E_Commerce_APIs.Application.Features.Users.Queries.GetUserById;
+
+public class GetUserByIdQuery : IRequest<Result<UserDto>>
+{
+    public Guid Id { get; set; }
+}
